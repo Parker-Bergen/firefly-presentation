352ceb546239d285f93747d23166c309d6303094
Parker Bergen <parkerb00@hotmail.com>
2023-04-16 13:23:58 -0400

    adding debug

--- app/api/logging/route.ts
+++ app/api/logging/route.ts
@@ -1,49 +1,49 @@
-import {PrismaClient } from '@prisma/client';
-import { NextRequest } from 'next/server';
-import { env } from 'process';
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server";
+import { env } from "process";
 
 export async function POST(request: NextRequest) {
-  const prisma = new PrismaClient()
-    try {
-        // Check if the request has a valid content type before parsing JSON
-        if (request.headers.get("content-type")?.includes("application/json")) {
-          const requestBody = await request.json();
-          //console.log(requestBody);
-          if (requestBody !== null && requestBody.key === env.APIKEY) {
-            const logData = {
-              broswer: requestBody.browser,
-              device: requestBody.device,
-              os: requestBody.os, 
-              ip: requestBody.ip,
-            };
+  const prisma = new PrismaClient();
+  try {
+    // Check if the request has a valid content type before parsing JSON
+    if (request.headers.get("content-type")?.includes("application/json")) {
+      const requestBody = await request.json();
+      //console.log(requestBody);
+      if (requestBody !== null && requestBody.key === env.APIKEY) {
+        const logData = {
+          broswer: requestBody.browser,
+          device: requestBody.device,
+          os: requestBody.os,
+          ip: requestBody.ip,
+        };
 
-            //console.log(logData);
-            const log = await prisma.log.create({
-              data: logData
-            });
-            return new Response(JSON.stringify(log))
-          }
-        } else {
-            return new Response("Invalid content type, JSON not parsed");
-        }
-      } catch (error) {
-        //console.log(error)
-        return new Response("Error while parsing JSON");
+        //console.log(logData);
+        const log = await prisma.log.create({
+          data: logData,
+        });
+        return new Response(JSON.stringify(log));
       }
-  }
-
-
-  export async function GET(request: NextRequest) {
-    const prisma = new PrismaClient()
-    const query = request.nextUrl.searchParams;
-    if(query){
-      const key = query.get("key");
-      if(key === env.APIKEY){
-        const data = await prisma.log.findMany();
-        //console.log(data);
-        return new Response(JSON.stringify(data));
-      }
+      return new Response("Invalid Key or request");
+    } else {
+      return new Response("Invalid content type, JSON not parsed");
     }
-
-    return new Response('Error, No Key')
+  } catch (error) {
+    //console.log(error)
+    return new Response("Error while parsing JSON");
   }
+}
+
+export async function GET(request: NextRequest) {
+  const prisma = new PrismaClient();
+  const query = request.nextUrl.searchParams;
+  if (query) {
+    const key = query.get("key");
+    if (key === env.APIKEY) {
+      const data = await prisma.log.findMany();
+      //console.log(data);
+      return new Response(JSON.stringify(data));
+    }
+  }
+
+  return new Response("Error, No Key");
+}

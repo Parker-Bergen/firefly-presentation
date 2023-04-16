1419ca865e31a6c705776a8fdbdae1e0490ba27f
Parker Bergen <parkerb00@hotmail.com>
2023-04-15 23:52:00 -0400

    Removed src dir and added styled components registry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
